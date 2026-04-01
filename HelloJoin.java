ae131aaf3cbe1f0e5e2be952bdc48eaf1faf02a9
Rangasamy V L <vlrangasamy111@gmail.com>
2026-04-01 22:48:41 +0530

    Implement HelloJoin class to greet users

---
+++ HelloJoin.java
@@ -0,0 +1,10 @@
+public class HelloJoin {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            String names = String.join(", ", args);
+            System.out.println("Hello " + names + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+    }
+}
